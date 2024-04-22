d136c0151e16accf84d5a1c42f14471bd6e68cf4
CojaONIX <cojaonix@gmail.com>
2024-04-22 20:23:05 +0200

    12.7 checkCredentials

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,15 +1,33 @@
 
+import USERS from '../Data/users.json';
+import {useState} from "react";
 
 const Login = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const checkCredentials = () => {
+        if(!(username.trim() && password.trim())) {
+            return;
+        }
+
+        USERS.forEach((user) => {
+            if(user.username === username && user.password === password) {
+                console.log('Pozdrav ' + username);
+            }
+        });
+    }
+
     return (
         <>
             <h1>Login</h1>
             <hr/>
-
+            <pre>{JSON.stringify(USERS, null, 2)}</pre>
             <form>
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <button type="button">Login</button>
+                <input onInput={ e => setUsername(e.currentTarget.value)} type="text" placeholder="Username" />
+                <input onInput={ e => setPassword(e.currentTarget.value)} type="password" placeholder="Password" />
+                <button onClick={checkCredentials} type="button">Login</button>
             </form>
         </>
     )
