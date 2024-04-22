0283b26ccffd84618aca99f90b494f5be06c64b5
CojaONIX <cojaonix@gmail.com>
2024-04-22 20:40:12 +0200

    12.9 login validation

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -6,24 +6,33 @@ const Login = () => {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [loginError, setLoginError] = useState(null);
 
     const checkCredentials = () => {
         if(!(username.trim() && password.trim())) {
+            setLoginError('Uneli ste pogresne podatke!');
             return;
         }
 
+        let foundUser = false;
         USERS.forEach((user) => {
             if(user.username === username && user.password === password) {
-                console.log('Pozdrav ' + username);
+                foundUser = true;
             }
         });
+
+        if(!foundUser) {
+            setLoginError('Ne postoji trazeni korisnik!');
+        } else {
+            setLoginError('Pozdrav '+username+'!');
+        }
     }
 
     return (
         <>
             <h1>Login</h1>
             <hr/>
-            <pre>{JSON.stringify(USERS, null, 2)}</pre>
+            <p>{loginError}</p>
             <form>
                 <input onInput={ e => setUsername(e.currentTarget.value)} type="text" placeholder="Username" />
                 <input onInput={ e => setPassword(e.currentTarget.value)} type="password" placeholder="Password" />
